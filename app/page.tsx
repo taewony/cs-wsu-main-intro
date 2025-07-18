097b2010fa783038cc18de45a299340ff0854a73
taewony <engchat@gmail.com>
2025-07-18 13:53:24 +0900

    re-design and fix error2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "@/components/HeroSection";
 import GuideSection from "@/components/GuideSection";
 import ImageSlider from "@/components/ImageSlider";
-import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-black dark:bg-black items-center relative">
-      <ThemeToggle />
       <HeroSection />
       <ImageSlider />
       <GuideSection />

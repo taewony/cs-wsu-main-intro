bea5a0e34eb0921fc80bc2757adb2ceb96d2ad2a
taewony <engchat@gmail.com>
2025-07-18 13:02:01 +0900

    re-design

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/cs-wsu-main-intro',
+  assetPrefix: '/cs-wsu-main-intro',
   images: {
     unoptimized: true,
   },

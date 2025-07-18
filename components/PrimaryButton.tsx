f4ef8b041e240cb63f6fa101bf55d8e474fcb70b
taewony <engchat@gmail.com>
2025-07-18 12:59:05 +0900

    re-design

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,7 +1,9 @@
 export default function PrimaryButton({ text }: { text: string }) {
   return (
-    <button className="bg-blue-600 text-white px-6 py-3 rounded-xl text-sm font-semibold hover:bg-blue-700 transition">
-      {text}
-    </button>
+    <a href="https://taewony.github.io/wsu-cs-homepage/">
+      <button className="bg-blue-600 text-white px-6 py-3 rounded-xl text-sm font-semibold hover:bg-blue-700 transition">
+        {text}
+      </button>
+    </a>
   );
 }
